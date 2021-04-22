ffa4dfe8ffe7ee95a129d5a72c5265bb6b4c73bc
cvargas <cristhian.avc21@gmail.com>
2021-04-21 22:47:07 -0500

    commit init

---
+++ src/main/java/com/cvargas/demo/DemoCvargasApplication.java
@@ -0,0 +1,13 @@
+package com.cvargas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCvargasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCvargasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvargas/demo/DemoCvargasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvargas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCvargasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
