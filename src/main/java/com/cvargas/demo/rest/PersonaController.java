308e4dd9176186aa5cb793650cbdbe098f93af7a
cvargas <cristhian.avc21@gmail.com>
2021-04-21 23:07:54 -0500

    Servicio rest segunda version

--- src/main/java/com/cvargas/demo/rest/PersonaController.java
+++ src/main/java/com/cvargas/demo/rest/PersonaController.java
@@ -28,48 +28,27 @@ public class PersonaController {
 	@Autowired
 	private PersonDao personDAO;
 	
-	
 	@GetMapping
 	public ResponseEntity<List<Person>> getPerson(){
 		List<Person> persons = personDAO.findAll();
 		return ResponseEntity.ok(persons);
-
-		/*Person person = new Person();
-		person.setId(1L);
-		person.setName("Cristhian");
-		person.setLastName("Vargas");
-		return ResponseEntity.ok(person);*/
-		//return null;
 	}
 	
 	@RequestMapping(value="{id}") 
 	public ResponseEntity<Person> getPersonById(@PathVariable("id") Long id){
-		//Optional nos protege de un valor null
 		Optional<Person> optionalPerson = personDAO.findById(id);
 		if(optionalPerson.isPresent()) {
 			return ResponseEntity.ok(optionalPerson.get());
 		}else {
 			return ResponseEntity.noContent().build();
+			//return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		//return ResponseEntity.ok(person);
-		//return null;
 	}
 	
-	/*@PostMapping
-	public ResponseEntity<Person> createPerson(@RequestBody Person person){
-		Person newPerson = personDAO.save(person);
-		return ResponseEntity.ok(newPerson);
-	}*/
-	
-	//@PostMapping("/tutorials")
 	@PostMapping
-	public ResponseEntity<Person> createPerson2(@RequestBody Person person) {
-		try {
-			System.out.println("Vamo aqui ------");
-			System.out.println(person);
-			
-			Person _person = personDAO
-					.save(person);
+	public ResponseEntity<Person> createPerson(@RequestBody Person person) {
+		try {			
+			Person _person = personDAO.save(person);
 			return new ResponseEntity<>(_person, HttpStatus.CREATED);
 			//return ResponseEntity.ok(_person);
 		} catch (Exception e) {
@@ -96,12 +75,5 @@ public class PersonaController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
-	
-	
-	//@GetMapping //localhost:8080
-	//@RequestMapping(value="hello", method=RequestMethod.GET) //localhost:8080/hello
-	public String hello() {
-		return "cvargas cuero";
-	}
+
 }
